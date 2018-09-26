2a0779606901dbed0c942f86d377d9aaacb69c54
yxw <1271035793@qq.com>
2018-09-26 18:25:05 +0800

    还原到了只有一行代码的文件

--- demo-test/src/main/java/cn/itheima/DemoTest.java
+++ demo-test/src/main/java/cn/itheima/DemoTest.java
@@ -3,7 +3,5 @@ package cn.itheima;
 public class DemoTest {
     public static void main(String[] args) {
         System.out.println("=============1111111111==================");
-		System.out.println("=============2222222222==================");
-		
     }
 }
